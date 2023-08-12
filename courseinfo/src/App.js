6a270c94a8d3dbaf26f32905faddb53f2b87c5a8
HackMaster-ai <moneyman17101994@gmail.com>
2023-08-12 21:09:40 +0100

    The final result of the exercises

---
+++ courseinfo/src/App.js
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <h1>{props.header}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} number of exercises : {props.number}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <Part name={props.name} number={props.number} />
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises : {props.total}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header header={course} />
+      <Content name={part1} number={exercises1}/>
+      <Content name={part2} number={exercises2}/>
+      <Content name={part3} number={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
