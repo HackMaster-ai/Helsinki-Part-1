8481c2bfd969764b505f4ac655a76d33d8fdba9a
HackMaster-ai <moneyman17101994@gmail.com>
2023-08-30 22:09:23 +0100

    1.6 part finished

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Header = (props) => {
+  const {title} = props
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{title}</h1>
+  )
 }
 
-export default App;
+const Button = (props) => {
+  const {name, func} = props
+  return (
+    <button onClick={func}>{name}</button>
+  )
+}
+
+const DisplayStat = (props) => {
+  const {name, value} = props
+  return (
+    <p>{name} : {value}</p>
+  )
+}
+
+const App = () => {
+  // enregistrer les clics de chaque bouton dans un état différent
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  function incrementGood() {
+    setGood(preValue => preValue + 1)
+  }
+
+  function incrementNeutral() {
+    setNeutral(preValue => preValue + 1)
+  }
+
+  function incrementBad() {
+    setBad(preValue => preValue + 1)
+  }
+
+  return (
+    <div>
+      <Header title={"Give feedback"}/>
+      <Button func={incrementGood} name={'Good'}/>
+      <Button func={incrementBad} name={'Bad'}/>
+      <Button func={incrementNeutral} name={'Neutral'}/>
+      <Header title={"Statistics"} />
+      <DisplayStat name={'Good'} value={good} />
+      <DisplayStat name={'Bad'} value={bad} />
+      <DisplayStat name={'Neutral'} value={neutral} />
+    </div>
+  )
+}
+
+export default App
