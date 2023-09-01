0a5401a0f0ca31a5c7db39afa7b1a7d9f69f5199
HackMaster-ai <moneyman17101994@gmail.com>
2023-09-01 19:47:47 +0100

    i don't know how to do it

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -11,28 +11,61 @@ const Button = (props) => {
 
 
 function App() {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
-  
+
   const [selected, setSelected] = useState(() => {
     return 0
   })
+
+  const [vote, setVote] = useState(() => {
+    return 0
+  })
+
+  const anecdotes = [
+    {
+      value: 'If it hurts, do it more often.',
+      votes: vote,
+    },
+    {
+      value: 'Adding manpower to a late software project makes it later!',
+      votes: vote
+    },
+    {
+      value: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      votes: vote
+    },
+    {
+      value: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      votes: vote
+    },
+    {
+      value: 'Premature optimization is the root of all evil.',
+      votes: vote
+    },
+    {
+      value: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      votes: vote
+    },
+    {
+      value: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+      votes: vote
+    }
+  ]
 
   function number() {
     setSelected(prevValue => Math.floor(Math.random() * anecdotes.length))
   }
 
+  function increaseVote() {
+    setVote(prevValue => prevValue + 1)
+    anecdotes[selected]['votes'] = vote
+  }
+
   return (
     <>
-    <p>{anecdotes[selected]}</p>
+    <p>{anecdotes[selected].value}</p>
+    <p>this anecdote has : {anecdotes[selected].votes}</p>
     <Button func={number} name={'new anecdote'} />
+    <Button func={increaseVote} name={'Vote'} />
     </>
   );
 }
