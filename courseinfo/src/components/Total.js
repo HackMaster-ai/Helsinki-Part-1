f9546c69f1703da045f45849e0fbbbcdaa4bf315
HackMaster-ai <moneyman17101994@gmail.com>
2023-09-11 21:18:06 +0100

    2.3 part finished

--- courseinfo/src/components/Total.js
+++ courseinfo/src/components/Total.js
@@ -2,15 +2,19 @@ const Total = (props) => {
     const {courses} = props
     const parts = courses.parts
 
-    let total = 0
+    let test = parts.reduce((s, p) => {
+        return s + p.exercises
+    }, 0)
 
-    for(let i = 0; i < parts.length; i++) {
-        total += parts[i].exercises
-    }
+    // let total = 0
+
+    // for(let i = 0; i < parts.length; i++) {
+    //     total += parts[i].exercises
+    // }
 
     return (
         <>
-            <h2>total of exercises is : {total}</h2>
+            <h2>total of exercises is : {test}</h2>
         </>
     )
 
